23eb12162902737cf91bff2077b5c9e73293499b
Tan Jun Ling <owentan712@gmail.com>
2022-05-08 00:55:50 +0800

    Add DAO method search by id

--- src/com/util/DAO.java
+++ src/com/util/DAO.java
@@ -12,9 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DAO {
-    private String jdbcURL = "jdbc:mysql://localhost:3306/ifarm";
-    private String username = "root";
-    private String password = "root";
+    private final String jdbcURL = "jdbc:mysql://localhost:3306/ifarm";
+    private final String username = "root";
+    private final String password = "root";
     private Connection connection;
     private PreparedStatement statement;
 
@@ -27,7 +27,7 @@ public class DAO {
         }
     }
 
-    public List<Plant> getPlantData() {
+    public Plant[] getPlantData() {
         List<Plant> plantList = new ArrayList<>();
         try {
             statement = connection.prepareStatement("SELECT * FROM plants");
@@ -43,10 +43,30 @@ public class DAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return plantList;
+        return plantList.toArray(new Plant[plantList.size()]);
     }
 
-    public List<Fertilizer> getFertilizerData() {
+    public Plant[] getPlantDataByFarmId(String farmId) {
+        List<Plant> plantList = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement("SELECT * FROM farm_plant INNER JOIN plants ON farm_plant.plant_id=plants._id where farm_id=?");
+            statement.setString(1, farmId);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                //mapping result set to plant
+                Plant plant = new Plant();
+                plant.set_id(result.getString("_id"));
+                plant.setName(result.getString("name"));
+                plant.setUnitType(result.getString("unitType"));
+                plantList.add(plant);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return plantList.toArray(new Plant[plantList.size()]);
+    }
+
+    public Fertilizer[] getFertilizerData() {
         List<Fertilizer> fertilizerList = new ArrayList<>();
         try {
             statement = connection.prepareStatement("SELECT * FROM fertilizers");
@@ -62,16 +82,36 @@ public class DAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return fertilizerList;
+        return fertilizerList.toArray(new Fertilizer[fertilizerList.size()]);
     }
 
-    public List<Pesticide> getPesticideData() {
-        List<Pesticide> pesticideList = new ArrayList<>();
+    public Fertilizer[] getFertilizerDataByFarmId(String farmId) {
+        List<Fertilizer> fertilizerList = new ArrayList<>();
         try {
-            statement = connection.prepareStatement("SELECT * FROM plants");
+            statement = connection.prepareStatement("SELECT * FROM farm_fertilizer INNER JOIN fertilizers ON farm_fertilizer.fertilizer_id=fertilizers._id where farm_id=?");
+            statement.setString(1, farmId);
             ResultSet result = statement.executeQuery();
             while (result.next()) {
-                //mapping result set to plant
+                //mapping result set to fertilizer
+                Fertilizer fertilizer = new Fertilizer();
+                fertilizer.set_id(result.getString("_id"));
+                fertilizer.setName(result.getString("name"));
+                fertilizer.setUnitType(result.getString("unitType"));
+                fertilizerList.add(fertilizer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return fertilizerList.toArray(new Fertilizer[fertilizerList.size()]);
+    }
+
+    public Pesticide[] getPesticideData() {
+        List<Pesticide> pesticideList = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement("SELECT * FROM pesticides");
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                //mapping result set to pesticide
                 Pesticide pesticide = new Pesticide();
                 pesticide.set_id(result.getString("_id"));
                 pesticide.setName(result.getString("name"));
@@ -81,7 +121,27 @@ public class DAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return pesticideList;
+        return pesticideList.toArray(new Pesticide[pesticideList.size()]);
+    }
+
+    public Pesticide[] getPesticideDataByFarmId(String farmId) {
+        List<Pesticide> pesticideList = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement("SELECT * FROM farm_pesticide INNER JOIN pesticides ON farm_pesticide.pesticide_id=pesticides._id where farm_id=?");
+            statement.setString(1, farmId);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                //mapping result set to pesticide
+                Pesticide pesticide = new Pesticide();
+                pesticide.set_id(result.getString("_id"));
+                pesticide.setName(result.getString("name"));
+                pesticide.setUnitType(result.getString("unitType"));
+                pesticideList.add(pesticide);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pesticideList.toArray(new Pesticide[pesticideList.size()]);
     }
 
     public List<Farmer> getFarmerData() {
@@ -90,13 +150,15 @@ public class DAO {
             statement = connection.prepareStatement("SELECT * FROM users");
             ResultSet result = statement.executeQuery();
             while (result.next()) {
-                //mapping result set to plant
+                //mapping result set to farmer
                 Farmer farmer = new Farmer();
                 farmer.set_id(result.getString("_id"));
                 farmer.setName(result.getString("name"));
                 farmer.setEmail(result.getString("email"));
                 farmer.setPhoneNumber(result.getString("phoneNumber"));
                 farmer.setPassword(result.getString("password"));
+                //need to reconfirm the name in farmer class
+                farmer.setFarmList(getFarmDataByFarmerId(farmer.get_id()));
                 farmerList.add(farmer);
             }
         } catch (SQLException e) {
@@ -105,7 +167,7 @@ public class DAO {
         return farmerList;
     }
 
-    public List<Farm> getFarmData() {
+    public Farm[] getFarmData() {
         List<Farm> farmList = new ArrayList<>();
         try {
             statement = connection.prepareStatement("SELECT * FROM farms");
@@ -116,27 +178,53 @@ public class DAO {
                 farm.set_id(result.getString("_id"));
                 farm.setName(result.getString("name"));
                 farm.setAddress(result.getString("address"));
+                farm.setPlants(getPlantDataByFarmId(farm.get_id()));
+                farm.setFertilizes(getFertilizerDataByFarmId(farm.get_id()));
+                farm.setPesticides(getPesticideDataByFarmId(farm.get_id()));
                 farmList.add(farm);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return farmList;
+        return farmList.toArray(new Farm[farmList.size()]);
+    }
+
+    public Farm[] getFarmDataByFarmerId(String farmerId) {
+        List<Farm> farmList = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement("SELECT * FROM user_farm INNER JOIN farms ON user_farm.farm_id=farms._id where user_id=?");
+            statement.setString(1, farmerId);
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                //mapping result set to farms
+                Farm farm = new Farm();
+                farm.set_id(result.getString("_id"));
+                farm.setName(result.getString("name"));
+                farm.setAddress(result.getString("address"));
+                farm.setPlants(getPlantDataByFarmId(farm.get_id()));
+                farm.setFertilizes(getFertilizerDataByFarmId(farm.get_id()));
+                farm.setPesticides(getPesticideDataByFarmId(farm.get_id()));
+                farmList.add(farm);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return farmList.toArray(new Farm[farmList.size()]);
     }
 
     public void insertActivityData(Activity activity) {
         try {
             statement = connection.prepareStatement("INSERT INTO activities('_id','farmId','userId','date','action','type','unit','quantity','row','field') VALUES(?,?,?,?,?,?,?,?,?,?)");
-            statement.setString(0,activity.get_id());
+            statement.setString(0, activity.get_id());
             statement.setString(1, activity.getFarmId());
             statement.setString(2, activity.getUserId());
-            statement.setString(3,activity.getDate());
-            statement.setString(4,activity.getAction());
-            statement.setString(5,activity.getType());
-            statement.setString(6,activity.getUnit());
-            statement.setDouble(7,activity.getQuantity());
-            statement.setInt(8,activity.getRow());
-            statement.setInt(9,activity.getField());
+            statement.setString(3, activity.getDate());
+            statement.setString(4, activity.getAction());
+            statement.setString(5, activity.getType());
+            statement.setString(6, activity.getUnit());
+            statement.setDouble(7, activity.getQuantity());
+            statement.setInt(8, activity.getRow());
+            statement.setInt(9, activity.getField());
             statement.executeUpdate();
             connection.commit();
         } catch (SQLException e) {
