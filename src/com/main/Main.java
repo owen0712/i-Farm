59f056ce83e68192fdc9362f418cb1c73da675c9
siayiheng0219 <81074268+siayiheng0219@users.noreply.github.com>
2022-05-19 16:32:35 +0800

    Update Main.java
    Add thread pool

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -5,17 +5,38 @@ import com.util.Timer;
 import com.util.TimerThread;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String[]args) throws InterruptedException {
+//        Random random = new Random();
+//        FarmerSimulator simulator = new FarmerSimulator();
+//        Farmer[]farmers=simulator.generateFarmers(random.nextInt(100)+100);
+//        for(Farmer farmer:farmers){
+//            Thread farmerThread=new Thread(farmer);
+//            farmerThread.start();
+//        }
+
         Random random = new Random();
+        int randFarmer = random.nextInt(100)+100;
+        ExecutorService farmerThreadPool = Executors.newFixedThreadPool(randFarmer);
         FarmerSimulator simulator = new FarmerSimulator();
-        Farmer[]farmers=simulator.generateFarmers(random.nextInt(100)+100);
+        Farmer[]farmers=simulator.generateFarmers(randFarmer);
+        Timer timer = new Timer();
+        timer.setDisasterTime(15);
+        TimerThread timerThread = new TimerThread(timer);
+        timerThread.start();
         for(Farmer farmer:farmers){
-            Thread farmerThread=new Thread(farmer);
-            farmerThread.start();
+            farmerThreadPool.execute(farmer);
         }
+        farmerThreadPool.shutdown();
+        while(!farmerThreadPool.isTerminated()){
+         //do something
+        }
+        timer.setEnd(true);
+        System.out.println("All thread have been terminated");
 
         // I did another version using threadPool see see
 //        Random random = new Random();
