de85173bfa306870de7a8ffcca1e3a85b7a30c5e
Law Wen Xuan <arhboy2000@gmail.com>
2022-05-20 10:53:32 +0800

    WIP

--- src/com/dataHandling/DataHandling.java
+++ src/com/dataHandling/DataHandling.java
@@ -10,35 +10,43 @@ import com.util.Timer;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class DataHandling implements Runnable {
 
     public static Queue<DataEntryWorker> processActivities;
-    private ExecutorService threadPool;
+    private static ExecutorService threadPool;
     private static volatile int activityId;
 
     public DataHandling() {
-        this.threadPool = Executors.newCachedThreadPool();
+        this.threadPool = Executors.newFixedThreadPool(100);
         this.processActivities = new LinkedList<DataEntryWorker>();
         this.activityId = 1;
     }
 
-    public synchronized static void addElementIntoQueue(Activity activity) {
-        activity.set_id("A"+activityId);
+    public synchronized static Future<Boolean> addElementIntoQueue(Activity activity) {
+        activity.set_id("A" + activityId);
         activityId++;
-        processActivities.add(new DataEntryWorker(activity));
+//        processActivities.add(new DataEntryWorker(activity));
+        return threadPool.submit(new DataEntryWorker(activity));
     }
 
     @Override
     public void run() {
         while (true) {
-            if (!processActivities.isEmpty()) {
-                System.out.println("CHICKYCHACHABOOMBOOM");
-                threadPool.submit(processActivities.poll());
-            } else if (Timer.isEnd()&&processActivities.isEmpty()) {
+//            if (!processActivities.isEmpty()) {
+//                threadPool.submit(processActivities.poll());
+//            } else if (Timer.isEnd()&&processActivities.isEmpty()) {
+//                break;
+//            }
+            if (Timer.isEnd()) {
                 break;
             }
         }
         threadPool.shutdown();
+        while (!threadPool.isTerminated()) {
+            //wait all farmer finish job
+        }
+        System.out.println("All data handling thread have been terminated");
     }
 }
