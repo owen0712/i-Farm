d6b36ec44a65a8770c4f5402c0853acb4258e6ea
ZhaoWei00 <75360593+ZhaoWei00@users.noreply.github.com>
2022-06-01 17:19:59 +0800

    Modify unitConvertor

--- src/com/util/UnitConverter.java
+++ src/com/util/UnitConverter.java
@@ -9,7 +9,10 @@ public class UnitConverter {
     public static String getConvertValueUnitString(double value, String unit){
         if (unit.equals("kg") || unit.equals("l") || unit.equals("packs(1kg)")){
             return (String.valueOf(value) + "kg");
-        } else {
+        }else if(unit.equals("packs(500g)")){
+            return (String.valueOf(value*500/1000) + "kg");
+        }
+        else {
             return (String.valueOf(value/1000.0) + "kg");
         }
     }
@@ -17,6 +20,8 @@ public class UnitConverter {
     public static double getConvertValue(double value, String unit){
         if (unit.equals("kg") || unit.equals("l") || unit.equals("packs(1kg)")){
             return value;
+        } else if(unit.equals("packs(500g)")){
+            return value*500/1000;
         } else {
             return value/1000.0;
         }
