2e3752add4b390227df8162e5ef3002ea6d0921b
Law Wen Xuan <arhboy2000@gmail.com>
2022-06-11 20:18:02 +0800

    Fix bugs + comments

--- src/com/dataVisualization/DataVisualizer.java
+++ src/com/dataVisualization/DataVisualizer.java
@@ -50,6 +50,7 @@ public class DataVisualizer {
 
         Scanner sc = new Scanner(System.in);
 
+        // let user choose which mode to display
         System.out.println("\nWelcome to ifarm: ");
         System.out.println("Below are the display mode:\n" +
                 "1. Display all activity logs for a target farm.\n" +
@@ -63,18 +64,11 @@ public class DataVisualizer {
         int mode = sc.nextInt();
         System.out.println();
 
+        // will break when user click 6
         while (true) {
+
+            // catch exception for input other than int
             try {
-                System.out.println("Below are the display mode:\n" +
-                        "1. Display all activity logs for a target farm.\n" +
-                        "2. Display all activity logs for a target farmer\n" +
-                        "3. Display all activity logs for a target farm and plant / fertilizer / pesticide\n" +
-                        "4. Display all activity logs for a target farm and plant / fertilizer / pesticide between date A and date B (inclusive)\n" +
-                        "5. Display summarized logs by plants, fertilizers and pesticides for a target farm and plant / fertilizer / pesticide between date A and date B (inclusive) for selected field and row number.\n" +
-                        "6. Exit Ifarm. ");
-                System.out.print("\nPlease choose a mode: ");
-                mode = sc.nextInt();
-                System.out.println();
                 switch (mode) {
                     case 1:
                         model1();
@@ -120,15 +114,20 @@ public class DataVisualizer {
         }
     }
 
+    // Mode 1
+    //1. Display all activity logs for a target farm.
     private void model1() {
         Scanner sc = new Scanner(System.in);
 
+        // print farm id for user to view
         printFarm();
 
+        // input farm id
         System.out.print("\nPlease choose a farm with farm id: ");
         String farmId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // Check farm id exist or not, if not, re-input again
         while (!checkFarmId(farmId.replace(" ", ""))) {
             System.out.println("Sorry, please enter correct farm id");
             System.out.print("Please try again: ");
@@ -136,7 +135,10 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // retrieve records from db based on farm id inputted
         List<Activity> farmActivityList = dao.getActivityByFarmId(farmId);
+
+        // print result
         if (farmActivityList.isEmpty()) {
             System.out.println("Sorry, not have such record.");
         } else {
@@ -145,18 +147,23 @@ public class DataVisualizer {
         }
     }
 
+    // Mode 2
+    // 2.	Display all activity logs for a target farmer
     private void model2() {
         Scanner sc = new Scanner(System.in);
 
+        // display farmer id to user
         System.out.println("Below is a list of farmer");
         for (int i = 0; i < farmers.length; i++) {
             System.out.print(farmers[i].get_id() + " " + farmers[i].getName() + "\n");
         }
 
+        // input farmer id
         System.out.print("\nPlease choose a farmer with farmer id: ");
         String farmerId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check farmer id exist or not, if not re-input again
         while (!checkFarmerId(farmerId.replace(" ", ""))) {
             System.out.println("Sorry, please enter correct farmer id");
             System.out.print("Please try again: ");
@@ -164,8 +171,10 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // retrieve records from database based on farmer id inputted
         List<Activity> farmerActivity = dao.getActivityByFarmerId(String.valueOf(farmerId));
 
+        // print result
         if (farmerActivity.isEmpty()) {
             System.out.println("Sorry, not have such record.");
         } else {
@@ -174,15 +183,20 @@ public class DataVisualizer {
         }
     }
 
+    // Mode 3
+    // 3.	Display all activity logs for a target farm and plant / fertilizer / pesticide
     private void model3() {
         Scanner sc = new Scanner(System.in);
 
+        // print farm id for user to view
         printFarm();
 
+        // input farm id
         System.out.print("\nPlease choose a farm with farm id: ");
         String farmId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check farm id exist or not, if not re-enter
         while (!checkFarmId(farmId.replace(" ", ""))) {
             System.out.println("Sorry, please enter correct farm id");
             System.out.print("Please try again: ");
@@ -190,10 +204,12 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // let user input which type they want to display
         System.out.print("Choose one type to display(plant/fertilizer/pesticide): ");
         String displayType = sc.nextLine();
         System.out.println();
 
+        // check input type equal to (plant/pesticide/fertilizer)
         while (!(displayType.equalsIgnoreCase("plant") || displayType.equalsIgnoreCase("fertilizer")
                 || displayType.equalsIgnoreCase("pesticide"))) {
             System.out.println("Sorry, please enter correct display type (plant/fertilizer/pesticide)");
@@ -202,12 +218,16 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // if equal to those 3 type, then print respective id for inputted type
+        // for the farm id
         printPlantFertilisePesticides(displayType, farmId);
 
+        // input (plant/fertilizer/pesticide) id
         System.out.print("\nPlease input an id of the type chosen(plant/fertilizer/pesticide): ");
         String chosenId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check id exist or not, if not then re-input
         while (!checkChosenId(chosenId)) {
             System.out.println("Sorry, please input correct Id of related display type");
             System.out.print("Please enter again: ");
@@ -215,10 +235,13 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // map chosen to respective (plant/fertilizer/pesticide) name
         String chosenName = this.chosenMap.get(chosenId);
 
+        // retrieve record from db
         List<Activity> farmAndTypeList = dao.getActivityByFarmerIdAndType(farmId, chosenName);
 
+        // print output
         if (farmAndTypeList.isEmpty()) {
             System.out.println("Sorry, not have such record.");
         } else {
@@ -227,15 +250,21 @@ public class DataVisualizer {
         }
     }
 
+    // Mode 4
+    // 4.	Display all activity logs for a target farm
+    // and plant / fertilizer / pesticide between date A and date B (inclusive)
     private void model4() {
         Scanner sc = new Scanner(System.in);
 
+        // print farm id for user to view
         printFarm();
 
+        // input farm id
         System.out.print("\nPlease choose a farm with farm id: ");
         String farmId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check farm id exist or not, if not, re-input again
         while (!checkFarmId(farmId.replace(" ", ""))) {
             System.out.println("Sorry, please enter correct farm id");
             System.out.print("Please try again: ");
@@ -243,12 +272,15 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // retrieve first and last record of the activity based on farm id
         firstLastActivity = dao.getFirstAndLastRecord(farmId);
 
+        // let user input which type they want to display
         System.out.print("Choose one type to display(plant/fertilizer/pesticide): ");
         String displayType = sc.nextLine();
         System.out.println();
 
+        // check input type equal to (plant/pesticide/fertilizer)
         while (!(displayType.equalsIgnoreCase("plant") || displayType.equalsIgnoreCase("fertilizer")
                 || displayType.equalsIgnoreCase("pesticide"))) {
             System.out.println("Sorry, please enter correct display type (plant/fertilizer/pesticide)");
@@ -257,12 +289,16 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // if equal to those 3 type, then print respective id for inputted type
+        // for the farm id
         printPlantFertilisePesticides(displayType, farmId);
 
+        // input (plant/fertilizer/pesticide) id
         System.out.print("\nPlease input an id of the type chosen(plant/fertilizer/pesticide): ");
         String chosenId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check id exist or not, if not then re-input
         while (!checkChosenId(chosenId)) {
             System.out.println("Sorry, please input correct Id of related display type");
             System.out.print("Please enter again: ");
@@ -270,21 +306,29 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // map id to name of chosen type
         String chosenName = this.chosenMap.get(chosenId);
 
+        //get date of first activity date
         this.startDate = firstLastActivity.get(0).getDate();
+
+        // get date of last activity of the farm
         this.endDate = firstLastActivity.get(1).getDate();
 
+        // input date A
         System.out.printf("Please input start date (year.month.date.hour.min.ss)(exp: %s): ", this.startDate);
         String dateStart = sc.nextLine();
         System.out.println();
 
+        // input date B
         System.out.printf("Please input end date (year.month.date.hour.min.ss)(exp: %s): ", this.endDate);
         String dateEnd = sc.nextLine();
         System.out.println();
 
+        // retrieve records from database
         List<Activity> farmAndTypeBetweenDateList = dao.getActivityByFarmerIdAndTypeBetweenDate(farmId, chosenName, dateStart, dateEnd);
 
+        // print output
         if (farmAndTypeBetweenDateList.isEmpty()) {
             System.out.println("Sorry, not have such record.");
         } else {
@@ -293,15 +337,22 @@ public class DataVisualizer {
         }
     }
 
+    // Mode 5
+    // 5.	Display summarized logs by plants, fertilizers and pesticides
+    // for a target farm and plant / fertilizer / pesticide
+    // between date A and date B (inclusive) for selected field and row number.
     private void model5() {
         Scanner sc = new Scanner(System.in);
 
+        // print farm id for user to view
         printFarm();
 
+        // input farm id
         System.out.print("\nPlease choose a farm with farm id: ");
         String farmId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check farmId exist or not, if not, re-enter again
         while (!checkFarmId(farmId.replace(" ", ""))) {
             System.out.println("Sorry, please enter correct farm id");
             System.out.print("Please try again: ");
@@ -309,12 +360,15 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // retrieve first and last activity based on farm id
         firstLastActivity = dao.getFirstAndLastRecord(farmId);
 
+        // let user input which type they want to display
         System.out.print("Choose one type to display(plant/fertilizer/pesticide): ");
         String displayType = sc.nextLine();
         System.out.println();
 
+        // check input type equal to (plant/pesticide/fertilizer)
         while (!(displayType.equalsIgnoreCase("plant") || displayType.equalsIgnoreCase("fertilizer")
                 || displayType.equalsIgnoreCase("pesticide"))) {
             System.out.println("Sorry, please enter correct display type (plant/fertilizer/pesticide)");
@@ -323,12 +377,16 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // if equal to those 3 type, then print respective id for inputted type
+        // for the farm id
         printPlantFertilisePesticides(displayType, farmId);
 
+        // input (plant/fertilizer/pesticide) id
         System.out.print("\nPlease input an id of the type chosen(plant/fertilizer/pesticide): ");
         String chosenId = sc.nextLine().toUpperCase();
         System.out.println();
 
+        // check id exist or not, if not then re-input
         while (!checkChosenId(chosenId)) {
             System.out.println("Sorry, please input correct Id of related display type");
             System.out.print("Please enter again: ");
@@ -336,34 +394,45 @@ public class DataVisualizer {
             System.out.println();
         }
 
+        // map id to name of chosen type
         String chosenName = this.chosenMap.get(chosenId);
 
+        // print how many field and row of the farm to user
         printFieldAndRowOfFarm(farmId);
 
-        System.out.print("Please select a field: ");
+        // input field
+        System.out.print("\nPlease select a field: ");
         int fieldSelected = sc.nextInt();
         System.out.println();
 
+        // input row
         System.out.print("Please select a row: ");
         int rowSelected = sc.nextInt();
         System.out.println();
 
         sc.nextLine();
 
+        // get date of first activity of the farm
         this.startDate = firstLastActivity.get(0).getDate();
+
+        // get date of last activity of the farm
         this.endDate = firstLastActivity.get(1).getDate();
 
+        // input date A
         System.out.printf("Please input start date (year.month.date.hour.min.ss)(exp: %s): ", this.startDate);
         String dateStart = sc.nextLine();
         System.out.println();
 
+        // input date B
         System.out.printf("Please input end date (year.month.date.hour.min.ss)(exp: %s): ", this.endDate);
         String dateEnd = sc.nextLine();
         System.out.println();
 
+        // retrieve record
         List<Activity> farmAndTypeBetweenDateListWithFieldRowList = dao.getActivityByFarmerIdAndTypeBetweenDateWithFieldRow(farmId,
-                displayType, dateStart, dateEnd, fieldSelected, rowSelected);
+                chosenName, dateStart, dateEnd, fieldSelected, rowSelected);
 
+        // Summarize all the data by using map
         if (farmAndTypeBetweenDateListWithFieldRowList.isEmpty()) {
             System.out.println("Sorry, not have such record.");
         } else {
@@ -382,10 +451,13 @@ public class DataVisualizer {
                     activityTypeMap.put(activity.getAction(), tempMap);
                 }
             }
+
+            // print result
             printSummaryMapMode5(activityTypeMap, fieldSelected, rowSelected);
         }
     }
 
+    // print farm id
     private void printFarm() {
         System.out.println("Below is a list of farm");
         for (int i = 0; i < Main.farms.length; i++) {
@@ -393,30 +465,36 @@ public class DataVisualizer {
         }
     }
 
+    // check farm id, return true if exist
     private boolean checkFarmId(String farmId) {
         List<String> farmsIdList = new ArrayList<>(Arrays.asList(this.farmsIdArr));
         return farmsIdList.contains(farmId);
     }
 
+    // check (plant/fertilizer/pesticide) id, return true if exist
     private boolean checkChosenId(String chosenId) {
         return this.chosenIdList.contains(chosenId);
     }
 
+    // check farmer id, return true if exist
     private boolean checkFarmerId(String farmerId) {
         List<String> farmerIdList = new ArrayList<>(Arrays.asList(this.farmerIdArr));
         return farmerIdList.contains(farmerId);
     }
 
+    // return index of the farm in map
     private int retrieveFarmIdIndex(String farmId) {
         return this.farmIdMap.get(farmId);
     }
 
+    // print field and row of certain farm based on farmId
     private void printFieldAndRowOfFarm(String farmId) {
         int indexFarm = this.retrieveFarmIdIndex(farmId);
         System.out.println("Below is the field and row of the farm " + farmId);
         System.out.println("Field: " + Main.farms[indexFarm].getField() + " Row: " + Main.farms[indexFarm].getRow());
     }
 
+    // Print (plant/fertilizer/pesticide) id
     private void printPlantFertilisePesticides(String displayType, String farmId) {
 
         switch (displayType.toLowerCase()) {
@@ -434,6 +512,7 @@ public class DataVisualizer {
         }
     }
 
+    // print all plant id of certain farm
     private void printPlantId(String farmId) {
         System.out.println("Below is the list of plant id planted inside the farm " + farmId);
         this.indexFarmId = retrieveFarmIdIndex(farmId);
@@ -442,12 +521,14 @@ public class DataVisualizer {
 
         for (int i = 0; i < plants.length; i++) {
             System.out.print(plants[i].get_id() + ", ");
+            System.out.print(plants[i].getName() + ", ");
             this.chosenIdList.add(plants[i].get_id());
             this.chosenMap.put(plants[i].get_id(), plants[i].getName());
         }
         System.out.println();
     }
 
+    // print all fertilizer id of certain farm
     private void printFertilizerId(String farmId) {
         System.out.println("Below is the list of fertilizer id used in the farm " + farmId);
         this.indexFarmId = retrieveFarmIdIndex(farmId);
@@ -462,6 +543,7 @@ public class DataVisualizer {
         System.out.println();
     }
 
+    // print all pesticide id of certain farm
     private void printPesticide(String farmId) {
         System.out.println("Below is the list of pesticide id used in the farm " + farmId);
         this.indexFarmId = retrieveFarmIdIndex(farmId);
@@ -476,6 +558,7 @@ public class DataVisualizer {
         System.out.println();
     }
 
+    // print result for mode 5
     private void printSummaryMapMode5(Map<String, Map<String, Double>> summaryMap, int field, int row) {
         for (Map.Entry<String, Map<String, Double>> entry : summaryMap.entrySet()) {
             String key1 = entry.getKey();
